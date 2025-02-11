8c407ad7019fef8c9d31eb9cae0c368a84605f25
gustavo.nunes <gustavonunes392@gmail.com>
2025-02-11 01:19:10 -0300

    feat: criando arquivos iniciais e dockersFile

---
+++ backend/src/main/java/com/backend/BenchMarks/BenchMarksApplication.java
@@ -0,0 +1,13 @@
+package com.backend.BenchMarks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenchMarksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenchMarksApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/benchPlanisa/Gustavo/GustavoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benchPlanisa.Gustavo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GustavoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
